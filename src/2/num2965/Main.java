0f2c77936f8e8f65a96d9bb0698ff04161b87925
Leapense <48317757+Leapense@users.noreply.github.com>
2021-01-05 17:59:00 +0900

    added num2965/Main.java

---
+++ src/2/num2965/Main.java
@@ -0,0 +1,19 @@
+package num2965;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+		
+		System.out.println(Math.max(b - a, c - b) - 1);
+		br.close();
+	}
+}
