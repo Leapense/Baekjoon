146020c29cc4fceb19247a89e2fa39d7f9927f72
Leapense <48317757+Leapense@users.noreply.github.com>
2021-01-05 14:45:29 +0900

    added num2921/Main.java

---
+++ src/2/num2921/Main.java
@@ -0,0 +1,12 @@
+package num2921;
+
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		System.out.println((int)(n * (n + 1) * (n + 2) / 2));
+	}
+}
