d594c4dc28de9d989f59a5bfda67e6f0c01cf148
Leapense <iso9661@gmail.com>
2026-01-05 13:39:51 +0900

    Added solution code of BOJ 32246

---
+++ 32246번: 빙고 막기/Main.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine().trim());
+        int ans = (N == 2) ? 3 : N;
+        System.out.println(ans);
+    }
+}
