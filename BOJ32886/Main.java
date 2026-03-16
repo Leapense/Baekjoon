729dcfc428081662f61aa482d8879c3e0e4a48ee
Leapense <iso9661@gmail.com>
2026-03-16 14:45:45 +0900

    Added solution code of BOJ 32886

--- BOJ32886/Main.java
+++ BOJ32886/Main.java
@@ -1,5 +1,25 @@
-package BOJ32886;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Main {
-    
+    private static String transform(String s) {
+        char[] arr = s.toCharArray();
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (char)('a' + (arr[i] - 'a' + 13) % 26);
+        }
+        return new String(arr);
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String action = br.readLine();
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder out = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            String s = br.readLine();
+            out.append(transform(s)).append('\n');
+        }
+
+        System.out.print(out);
+    }
 }
